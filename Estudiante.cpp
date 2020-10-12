b31e5aa5a34e31b930c58c1111fe8614bc5776a0
sergiop <sergiojrg2015@gmail.com>
2020-10-11 19:24:39 -0600

    primer_commit

---
+++ Estudiante.cpp
@@ -0,0 +1,23 @@
+#include "Persona.cpp"
+#include <iostream>
+using namespace std; 
+
+class Estudiante:Persona{
+    //atributos 
+    private: string Carne; 
+    public: 
+    //constructor 
+        Estudiante(string nom, string ape, int tel, string n):Persona(nom,ape,tel){
+            Carne=n; 
+        }
+//modificar (set)
+void setCarne(string n){nit=n;}
+void setNombres(string nom){nombres=nom;}
+void setApellidos(string ape){apellidos=ape;}
+void setTelefono(int tel){telefono=tel;}
+//mostrar (get)
+    string getCarne(){return Carne;}
+    string getNombre(){return nombre;}
+    
+
+}
---
+++ Persona.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std; 
+
+class Persona{
+    //atributos 
+    protected : string Nombre, Apellidos; 
+                int Telefonos; 
+    protedted : 
+        persona(string nom, string ape, int tel){
+            Nombres=nom; 
+            Apellidos=ape; 
+            Telefonos=tel; 
+        }
+//metodos de accion 
+void agregar(); 
+void mostrar(); 
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
