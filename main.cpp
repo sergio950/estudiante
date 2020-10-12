0547596f1bf3d94f6639c89bc6bde11f598450d4
sergiop <sergiojrg2015@gmail.com>
2020-10-11 20:49:30 -0600

    primer_commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,31 @@
+#include "Estudiante.cpp"
 #include <iostream>
+using namespace std; 
+main(){
+string carne,nombres,apellidos; 
+int telefono; 
+cout<<"Ingrese carne; ";
+cin>>carne; 
+cout<<"Ingrese nombres; "; 
+cin>>nombres; 
+cout<<"Ingrese apellidos; ";
+cin>>apellidos; 
+cout<<"Ingrese telefono; ";
+cin>>telefono; 
+
+Estudiante obj = Estudiante(nombres,apellidos,telefono,carne); 
+obj.mostrar();
+
+cout<<"-------------Modificar--------------"<<endl;
+cout<<"Ingrese carne; ";
+cin>>carne; 
+cout<<"Ingrese nombres; ";
+cin>>nombres; 
+cout<<"Ingrese apellidos; "; 
+cin>>apellidos; 
+cout<<"Ingrese telefono; "; 
+cin>>
+     
+}
 
 
